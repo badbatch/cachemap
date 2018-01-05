50b5f4dcfa37b34d6680fed91e0b91b7dd21e96f
Dylan Aubrey <dylanaubrey@gmail.com>
2018-01-05 12:24:38 +0000

    Updating build config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,37 @@
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const { resolve } = require('path');
 const webpack = require('webpack');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const webpackConfig = require('./webpack.config.base');
+
+webpackConfig.module.rules.unshift({
+  include: [
+    resolve(__dirname, 'src'),
+  ],
+  test: /\.tsx?$/,
+  use: [{
+    loader: 'awesome-typescript-loader',
+    options: {
+      babelCore: '@babel/core',
+      transpileOnly: true,
+      useBabel: true,
+    },
+  }],
+});
+
+webpackConfig.plugins.push(
+  new webpack.SourceMapDevToolPlugin({
+    filename: '[name].js.map',
+    test: /\.(tsx?|jsx?)$/,
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    sourceMap: true,
+  }),
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'disabled',
+    generateStatsFile: true,
+    statsFilename: './bundle/stats.json',
+  }),
+);
 
 module.exports = {
   entry: {
@@ -14,51 +44,5 @@ module.exports = {
     library: 'Cachemap',
     libraryTarget: 'umd',
   },
-  module: {
-    rules: [{
-      include: [
-        resolve(__dirname, 'src'),
-        resolve(__dirname, 'test'),
-      ],
-      test: /\.tsx?$/,
-      use: [{
-        loader: 'awesome-typescript-loader',
-        options: {
-          babelCore: '@babel/core',
-          transpileOnly: true,
-          useBabel: true,
-        },
-      }],
-    }, {
-      test: /.worker\.js$/,
-      use: {
-        loader: 'worker-loader',
-      },
-    }],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-  },
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      TEST_ENV: !!process.env.TEST_ENV,
-      WEB_ENV: true,
-    }),
-    new webpack.LoaderOptionsPlugin({
-      debug: true,
-    }),
-    new webpack.SourceMapDevToolPlugin({
-      filename: '[name].js.map',
-      test: /\.(tsx?|jsx?)$/,
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'disabled',
-      generateStatsFile: true,
-      statsFilename: './bundle/stats.json',
-    }),
-    new LodashModuleReplacementPlugin(),
-  ],
+  ...webpackConfig,
 };
