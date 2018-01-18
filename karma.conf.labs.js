060997e0d506a3834538d9d165fe290a95662faf
Dylan Aubrey <dylanaubrey@gmail.com>
2018-01-18 08:43:44 +0000

    Updating karma config.

--- karma.conf.labs.js
+++ karma.conf.labs.js
@@ -1,53 +1,5 @@
+const customLaunchers = require('./custom-launchers.json');
 const webpackConfig = require('./webpack.config.labs');
-
-const customLaunchers = {
-  sl_chrome: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Windows 10',
-    version: 'latest',
-  },
-  sl_firefox: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Windows 10',
-    version: 'latest',
-  },
-  // sl_ie: {
-  //   base: 'SauceLabs',
-  //   browserName: 'internet explorer',
-  //   platform: 'Windows 7',
-  //   version: 'latest',
-  // },
-  // sl_edge: {
-  //   base: 'SauceLabs',
-  //   browserName: 'MicrosoftEdge',
-  //   platform: 'Windows 10',
-  //   version: 'latest',
-  // },
-  // sl_safari: {
-  //   base: 'SauceLabs',
-  //   browserName: 'safari',
-  //   platform: 'macOS 10.12',
-  //   version: 'latest',
-  // },
-  // sl_ios: {
-  //   base: 'SauceLabs',
-  //   browserName: 'Safari',
-  //   deviceName: 'iPhone Simulator',
-  //   deviceOrientation: 'portrait',
-  //   platformName: 'iOS',
-  //   platformVersion: '11.0',
-  // },
-  // sl_android: {
-  //   base: 'SauceLabs',
-  //   browserName: 'Chrome',
-  //   deviceName: 'Android Emulator',
-  //   deviceOrientation: 'portrait',
-  //   platformName: 'Android',
-  //   platformVersion: '6',
-  // },
-};
 
 module.exports = (config) => {
   config.set({
@@ -74,7 +26,7 @@ module.exports = (config) => {
     preprocessors: {
       'test/specs/index.ts': ['webpack', 'sourcemap'],
     },
-    reporters: ['progress', 'saucelabs'],
+    reporters: ['saucelabs'],
     sauceLabs: {
       startConnect: false,
       testName: 'Cachemap browser unit tests',
