ff1724025edbb4578d65d5422eeb476244f44354
Dylan Aubrey <dylanaubrey@gmail.com>
2018-01-20 22:59:05 +0000

    Updaitng karma config.

--- karma.conf.labs.js
+++ karma.conf.labs.js
@@ -4,6 +4,10 @@ const webpackConfig = require('./webpack.config.labs');
 let reporters = ['dots'];
 
 const sauceLabs = {
+  connectOptions: {
+    noSslBumpDomains: 'all',
+    sharedTunnel: true,
+  },
   startConnect: false,
   testName: 'Cachemap browser unit tests',
   tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
@@ -13,12 +17,8 @@ if (process.env.LOCAL_ENV) {
   require('dotenv').config(); // eslint-disable-line
   reporters = ['mocha'];
   sauceLabs.startConnect = true;
-  sauceLabs.connectOptions = {
-    logfile: '-',
-    noSslBumpDomains: 'all',
-    sharedTunnel: true,
-    verbose: true,
-  };
+  sauceLabs.connectOptions.logfile = '-';
+  sauceLabs.connectOptions.verbose = true;
 }
 
 module.exports = (config) => {
