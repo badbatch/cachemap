c7d7807233ec13884b20a6ba40d1e9ce92256253
Dylan Aubrey <dylan.aubrey@gmail.com>
2018-03-31 22:01:08 +0100

    Exposing typescript types.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-import { ClientStoreTypes, ConstructorArgs, Metadata, ServerStoreTypes } from "./types";
-export type CachemapClientStoreTypes = ClientStoreTypes;
+import {
+  ClientStoreTypes,
+  ConstructorArgs,
+  ExportResult,
+  ImportArgs,
+  Metadata,
+  ServerStoreTypes,
+} from "./types";
+
 export type CachemapArgs = ConstructorArgs;
+export type CachemapClientStoreTypes = ClientStoreTypes;
+export type CachemapExportResult = ExportResult;
+export type CachemapImportArgs = ImportArgs;
 export type CachemapMetadata = Metadata;
 export type CachemapServerStoreTypes = ServerStoreTypes;
 export { DefaultCachemap } from "./default-cachemap";
