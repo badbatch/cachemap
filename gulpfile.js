b272764bac445dab4aa4a4b99ca570717a345092
Dylan Aubrey <dylan.aubrey@gmail.com>
2018-08-07 23:47:53 +0100

    More config changes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,7 @@
 const gulp = require('gulp');
-const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
-const tslint = require('gulp-tslint');
 const typedoc = require('gulp-typedoc');
-const ts = require('gulp-typescript');
-const merge = require('merge-stream');
-const { Linter } = require('tslint');
 
 const sources = ['**/*.ts', '!**/*.test.*', '!**/__test__/**'];
-
-gulp.task('main', () => {
-  const tsProject = ts.createProject('tsconfig.json', { module: 'commonjs' });
-
-  return gulp.src(sources)
-    .pipe(sourcemaps.init())
-    .pipe(tsProject())
-    .pipe(babel())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./lib/main'))
-    .on('error', () => process.exit(1));
-});
-
-gulp.task('types', () => {
-  const tsProject = ts.createProject('tsconfig.json', { declaration: true, module: 'commonjs', removeComments: true });
-  const transpiled = gulp.src(sources).pipe(tsProject());
-  const copied = gulp.src(['src/**/*.d.ts']);
-
-  return merge(transpiled.dts, copied)
-    .pipe(gulp.dest('lib/types'))
-    .on('error', () => process.exit(1));
-});
-
-gulp.task('module', () => {
-  const tsProject = ts.createProject('tsconfig.json');
-
-  return gulp.src(sources)
-    .pipe(sourcemaps.init())
-    .pipe(tsProject())
-    .pipe(babel())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('lib/module'))
-    .on('error', () => process.exit(1));
-});
-
-gulp.task('browser', () => {
-  const tsProject = ts.createProject('tsconfig.json');
-
-  return gulp.src(sources)
-    .pipe(sourcemaps.init())
-    .pipe(tsProject())
-    .pipe(babel())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('lib/browser'))
-    .on('error', () => process.exit(1));
-});
-
-gulp.task('type-check', () => {
-  const tsProject = ts.createProject('tsconfig.json', { noEmit: true });
-
-  gulp.src(sources)
-    .pipe(tsProject())
-    .on('error', () => process.exit(1));
-});
-
-gulp.task('tslint', () => {
-  gulp.src(sources)
-    .pipe(tslint({
-      configuration: 'tslint.json',
-      fix: true,
-      formatter: 'stylish',
-      program: Linter.createProgram('tsconfig.json'),
-    }))
-    .pipe(tslint.report())
-    .on('error', () => process.exit(1));
-});
 
 gulp.task('document', () => gulp.src(sources)
   .pipe(typedoc({
